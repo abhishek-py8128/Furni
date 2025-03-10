b61126a0887aee06f8021640f4166e137494d2be
abhishek-py8128 <abhitapaniya@gmail.com>
2025-03-10 14:16:04 +0530

    Change Debug Settngs

--- Furni/settings.py
+++ Furni/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--!u!*a0(qm$n87zhs_=^h$u$&d8m__!xu$2h*d8+3&b!ia1l&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this middleware
 ]
 
 ROOT_URLCONF = 'Furni.urls'
