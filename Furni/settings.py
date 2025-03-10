40327c8313473a637a53fee859438fe5369d4251
abhishek-py8128 <abhitapaniya@gmail.com>
2025-03-10 13:59:51 +0530

    Change Speling Mistake

--- Furni/settings.py
+++ Furni/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Furni.wsgi.application'
+WSGI_APPLICATION = 'Furni.wsgi.application' 
 
 
 # Database
--- Furni/wsgi.py
+++ Furni/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'furni.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Furni.settings')
 
 application = get_wsgi_application()
