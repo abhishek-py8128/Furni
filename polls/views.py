d2fe802074f3449c7d349224c8703968b1bc493f
abhishek-py8128 <abhitapaniya@gmail.com>
2025-03-12 13:54:45 +0530

    Add Changes

--- polls/views.py
+++ polls/views.py
@@ -480,7 +480,7 @@ def payment_cart(request) :
         print(amount)
 
         # How to create payment Id
-        client = razorpay.Client(auth=("rzp_test_UkmXzU0ru6Xnon","1YtBsuIoM1TWDbLTTo3JGpRZ"))
+        client = razorpay.Client(auth=("rzp_test_bHGARzasqPyY7j","1Ae7bF0U1f1Z9m2FZzD3RhR0"))
         # payment id genrate
         payments = client.order.create({'amount':amount, 'currency':'INR', 'payment_capture':1})
         print('payment',payments)
